22f57baa5a22889d6855c92617d57ad98a3babb6
Tom Smeding <tomsmeding@users.noreply.github.com>
2016-08-28 15:53:24 +0200

    Improve range and speed of stats funcs in math.c

--- lib/math.c
+++ lib/math.c
@@ -13,19 +13,50 @@ unsigned int fact(unsigned int num) {
 	}
 }
 
-unsigned int binomial(int a, int b) {
-	return fact(a) / (fact(b) * fact(a - b));
+unsigned int binomial(int n, int k) {
+	if (k > n / 2) k = n - k;
+	if (k == 0) return 1;
+	unsigned int res = 1;
+	for (int i = k + 1; i <= n; i++) {
+		res = i * res / (i - k);
+	}
+	return res;
 }
 
 float binompdf(int n, float p, int k) {
 	return binomial(n, k) * powf(p, k) * powf(1-p, n-k);
 }
 
+//assumes 0<=value<=trials, 0.0<=probability<=1.0
 float binomcdf(int trials, float probability, int value) {
-	float sum = 0.0f;
-	int i;
-	for (i = 0; i < value; i++) {
-		sum += binompdf(trials, probability, i);
+	if (value == 0) {
+		return (float)value;
+	}
+	if (trials == 1 || probability == 0 || probability == 1) {
+		return 1 - probability;
+	}
+	if (trials == 0) {
+		return 1.0f;
+	}
+	int pascal[trials - 1];
+	pascal[0] = 2;
+	for(int i = 2; i < trials; i++){
+		pascal[i - 1] = pascal[i - 2] + 1;
+		for(int j = i - 2; j > 0; j--) {
+			pascal[j] += pascal[j - 1];
+		}
+		pascal[0]++;
+	}
+	float sum = 0.0f, pk = 1.0f, qnk = powf(1 - probability, trials);
+	if (value > 0) {
+		sum = qnk;
+		pk *= probability;
+		qnk /= 1 - probability;
+	}
+	for(int i = 1; i < value; i++) {
+		sum += pascal[i - 1] * pk * qnk;
+		pk *= probability;
+		qnk /= 1 - probability;
 	}
 	return sum;
 }
